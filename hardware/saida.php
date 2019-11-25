5726e63a669345ea61ad2c67352e24055ac07bec
welber Martins <welber.aguiar@gmail.com>
2019-11-25 10:27:33 -0300

    arquivos do estoque

---
+++ hardware/saida.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Estoque TI</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/style.css">
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <!-- Bootstrap datepicker -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap-datepicker.css">
+  
+</head>
+
+<body>
+<!-- INICIO DO MENU -->
+<div class="wrapper">
+    <?php include "../menu/menu.php"; ?>
+
+
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span></span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Logout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- FINAL DO MENU -->
+            <div class="container-fluid">
+                <form>
+                      <div class="form-row">
+                       <div class="form-group col-md-4">
+                          <label for="inputEstado">Dispositivo</label>
+                          <select id="inputEstado" class="form-control">
+                            <option selected>Escolher...</option>
+                            <option>...</option>
+                          </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                          <label for="inputEstado">Descrição</label>
+                          <select id="inputEstado" class="form-control">
+                            <option selected>Escolher...</option>
+                            <option>...</option>
+                          </select>
+                        </div>
+                        <div class="form-group col-md-3">
+                        <label for="exampleFormControlSelect1">Quantidade</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-2">
+                        <label >Data:</label>
+                            <div class='input-group date data_formato'  data-provide="datetimepicker5" >
+                                <input type='text' class="form-control" name="data" id="data" />
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-calendar"></span>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="form-group col-md-3">
+                          <label for="inputEstado">Setor</label>
+                          <select id="inputEstado" class="form-control">
+                            <option selected>Escolher...</option>
+                            <option>...</option>
+                          </select>
+                        </div>
+
+                        <div class="form-group col-md-3">
+                          <label for="inputCity">Patrimonio</label>
+                          <input type="text" class="form-control" id="inputCity">
+                   
+                        </div>
+
+                      <div class="form-group col-md-7">
+                          <label for="exampleFormControlTextarea1">Observação</label>
+                          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </div>
+
+                      <br><br>
+                     <center>
+                       <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                     </center>
+                      
+                </form>
+
+            </div>
+        </div>
+</div>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  <script src="../bootstrap/js/bootstrap-datepicker.min.js" ></script>
+  <script src="../bootstrap/locales/bootstrap-datepicker.pt-BR.min.js" ></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+
+    <script>
+            $('.data_formato').datepicker({
+              weekStart: 1,
+              todayBtn: 1,
+              autoclose: 1,
+              todayHighlight: 1,
+              forceParse: 0,
+              showMeridian: 1,
+              language:"pt-BR"
+            });
+       </script>
+</body>
+
+</html>
---
+++ menu/menu.php
@@ -0,0 +1,93 @@
+<div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>ESTOQUE TI</h3>
+                <strong>TI</strong>
+            </div>
+
+            <ul class="list-unstyled components">
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="fas fa-home"></i>
+                        Home
+                    </a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Home 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageHard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="fas fa-microchip"></i>
+                        Hardware
+                    </a>
+                    <ul class="collapse list-unstyled" id="pageHard">
+                        <li>
+                            <a href="hardware/index.php">Entrada</a>
+                        </li>
+                        <li>
+                            <a href="hardware/saida.php">Saída</a>
+                        </li>
+                        <li>
+                            <a href="#">Total de Hardware</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageSoft" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="fas fa-braille"></i>
+                        Software
+                    </a>
+                    <ul class="collapse list-unstyled" id="pageSoft">
+                       
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageNota" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="fas fa-calculator"></i>
+                        Nota Fiscais
+                    </a>
+                    <ul class="collapse list-unstyled" id="pageNota">
+                        <li>
+                            <a href="#">Adicionar Nota</a>
+                        </li>
+                        <li>
+                            <a href="#">Total de Notas</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageAdm" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="fas  fa-chevron-circle-up"></i>
+                        Admin
+                    </a>
+                    <ul class="collapse list-unstyled" id="pageAdm">
+                        <li>
+                            <a href="#">Adicionar Dispositivo</a>
+                        </li>
+                        <li>
+                            <a href="#">Excluir Dispositivo</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+<!--
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+                </li>
+                <li>
+                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+                </li>
+            </ul>
+        </nav>
+-->
+</div>
